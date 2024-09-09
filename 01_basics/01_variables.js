1b42985425b516ed676e0be0bc0eddb95d314693
Aman Maddhesia <60521225+Amanskn@users.noreply.github.com>
2024-09-09 07:12:15 +0000

    done with datatypes in JS

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId =90;
+let accountEmail = "aman@gmail.com";
+var accountPassword = "aman";
+accountCity = "Khalilabad"
+// accountId =90;   // Not allowed
+// Avoid using var variables
+
+console.table([accountCity,accountEmail,accountId,accountPassword])
+console.log(BigInt(34),34)
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+// There are two category of datatypes in JS
+// 1. Primitive datatype
+// 2. Non-Primitive datatype
+
+// ********** Primitive datatypes are as follows:-
+// string       ====> typeof "Aman" is string
+// boolean      ====> typeof true is boolean
+// number       ====> typeof 23 is number
+// undefined    ====> typeof undefined is undefined
+// null         ====> typeof null is object
+// bigint       ====> typeof 23n is bigint
+// symbol       ====> typeof Symbol(23) is symbol
+
+// *********** Non -primitive datatypes are as follows:-
+// 1. object    ====> typeof {} is object                    
+// 2. array     ====> typeof [] is object
+// 3. function  ====> typeof function fun(){} is function
