ec5febf7a2b8967f0014ab88f6b858c143bacf55
Amanskn <29maddhesiaaman@gmail.com>
2024-09-13 11:16:09 +0530

    done with nums and maths

---
+++ 01_basics/07 numsAndMaths.js
@@ -0,0 +1,31 @@
+// let num1 = 902;
+// let num2 = new Number("90883433215");
+// console.log(num1);
+// console.log("toString()", num2.toString());
+// console.log(num2.toExponential(3));
+// console.log(num2.toFixed(4));
+// console.log(num2.toLocaleString());
+// console.log(num2.valueOf());
+// console.log(num2.toPrecision());
+// console.log(num2);
+
+// ++++++++++++++++++++++++++ Math +++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.abs(94));
+// console.log(-(-564));
+// console.log(Math.sqrt(81));
+// console.log("$$", Math.round(34.54));
+// console.log(Math.ceil(34.54));
+// console.log(Math.floor(34.54));
+// console.log(3 ** 2);
+// console.log(Math.pow(3, 2));
+// console.log(Math.min(4, 1.6, 7, 8));
+// console.log(Math.max(4, 6, 7, 58));
+
+console.log(Math.floor(Math.random() * 7)); // 0 to 6
+console.log(Math.floor(Math.random() * 8)); // 0 to 7
+console.log(Math.floor(Math.random() * 6) + 1); // 1 to 6
+let max = 67;
+let min = 32;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
