9f86a933a2e0c1f6aa7412ed081321794a2445f2
Amanskn <29maddhesiaaman@gmail.com>
2024-09-14 14:50:05 +0530

    done with function intro

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,22 @@
+// function fun() {
+//   console.log("Inside fun6 function");
+// }
+// console.log(fun());
+
+// ============Function to add two numbers
+// below function has default parameters syntax
+// function addTwoNumber(num1 = 0, num2 = 0) {
+//   return Number(num1) + Number(num2);
+// }
+// console.log(addTwoNumber());
+
+//================ Function using rest(...) parameters
+// function calculateCartPrice(...cartItemsPrice) {
+//   let totalPrice = 0;
+//   cartItemsPrice.forEach((value, index, arr) => {
+//     console.log("##", index, arr);
+//     totalPrice += value;
+//   });
+//   return totalPrice;
+// }
+// console.log(calculateCartPrice(100, 200, 500, 400));
